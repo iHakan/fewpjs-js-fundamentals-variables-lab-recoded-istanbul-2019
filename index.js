f5e26b0c46d8cdd235e37e2dc24b409b7d0334d5
Hakan Yildiz <hakan@re-coded.com>
2020-01-28 09:01:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName = "Scuber";
 let mostProfitableNeighborhood = "Chelsea";
 let companyCeo = "Susan Smith";
+=======
+const companyName = "Scuber";
+>>>>>>> 71ca3a947cf1f367b3f8f9832d700a7c55a6dd2e
